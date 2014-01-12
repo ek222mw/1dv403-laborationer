74a4454cc03111ae77a34bc408ccac507b74e954
Emil Karlsson <ek222mw@student.lnu.se>
2014-01-12 11:58:42 -0500

    uppdatering

--- 5-pwd/script.js
+++ 5-pwd/script.js
@@ -7,7 +7,20 @@ var pwd =
     
     init: function()
     {
-        
+      document.getElementById("date").innerHTML = formatAMPM();
+
+        function formatAMPM() {
+        var date = new Date();
+        var hours = date.getHours();
+        var days = date.getDay(); 
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        var strTime = date + ' ' + hours + ':' + minutes + ' ' + ampm;
+        return strTime;
+        }   
     var popup = document.getElementById("newpopup");
     
      popup.onclick = function(){
@@ -24,7 +37,7 @@ var pwd =
      }
     
 },
-// Modal-popupfönster som bekräftar köpet en gång till.
+// Modal-popupfönster som visar bilder.
     popupWindow: function(){
                         
         // Skapar de olika elementen som representerar popupfönstret.
@@ -35,6 +48,8 @@ var pwd =
         var divpopupbild = document.createElement("div");
         var divtop = document.createElement("div");
         var img = document.createElement("img");
+        var popupimg = document.createElement("img");
+        var clock = document.createElement("div");
         
         console.log(divpopup);
 
@@ -44,14 +59,19 @@ var pwd =
         divtop.id = "divtop";
         img.id = "load";
         h2.id ="h2";
+        popupimg.id ="popupimg";
+        clock.id ="clock";
         main.appendChild(modalpop);
+        main.appendChild(clock);
          
         
         
         
         img.setAttribute("src", "pics/ajax-loader.gif");
+        popupimg.setAttribute("src","pics/thumbnail.gif");
         divtop.appendChild(h2);
-        h2.appendChild(document.createTextNode("Imageviewer"));
+        h2.appendChild(document.createTextNode("Image Viewer"));
+        divtop.appendChild(popupimg);
         
         
         divpopup.appendChild(img);
@@ -68,24 +88,31 @@ var pwd =
         main.parentNode.insertBefore(divpopup, main.nextSibling);
         main.parentNode.insertBefore(divtop, main.nextSibling);
         var url = "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/";
+        
+        
         var myCallback = function(data){
             var imagesarray = [];
-             imagesarray = JSON.parse(data);
-          
-            console.log("hej",imagesarray);
-            console.log(divpopup);
+            imagesarray = JSON.parse(data);
             img.parentNode.removeChild(img);
+            var thumbSize = maxWidthHeightThumb(imagesarray);
+            console.log(thumbSize);
+            var thumbHeight = thumbSize.height;
+            console.log("höjd",thumbHeight);
+            var thumbWidth = thumbSize.width;
+            console.log("bredd",thumbWidth);
         for(var i = 0; i<imagesarray.length; i++)
         {
-           changeimage(imagesarray,i);
-            
+           changeimage(imagesarray,i, thumbWidth,thumbHeight);
+           
             
         }
         };
-        var changeimage = function(imagesarray,i)
+        
+        var changeimage = function(imagesarray,i,thumbWidth,thumbHeight)
         {
-             var klickimg = document.createElement("a")
-            klickimg.setAttribute("href","#");
+             var klickimg = document.createElement("a");
+             console.log("bredd2",thumbWidth,"höjd2", thumbHeight);
+            klickimg.setAttribute("href","#","width",thumbWidth,"height", thumbHeight);
             var loadedimg = document.createElement("img");
             klickimg.className = "atag";
             loadedimg.className = "imgtag";
@@ -94,11 +121,38 @@ var pwd =
             klickimg.appendChild(loadedimg);
             divpopup.appendChild(klickimg);
             loadedimg.setAttribute("src", imagesarray[i].thumbURL);
+            klickimg.style.height = thumbHeight + "px";
+            klickimg.style.width = thumbWidth + "px;";
+            
             klickimg.onclick = function()
             {
-                loadedimg.setAttribute("src",imagesarray[i].URL)
+                var body = document.getElementById("body");
+                body.style.backgroundImage = "url(" + imagesarray[i].URL + ")";
             }
         }
+        var maxWidthHeightThumb = function(imagesarray)
+        {
+            var width = 0;
+            var height = 0;
+            for(var i = 0; i<imagesarray.length; i++)
+            {
+                if(imagesarray[i].thumbWidth > width)
+                {
+                    width = imagesarray[i].thumbWidth;
+                }
+            
+                if(imagesarray[i].thumbHeight > height)
+                {
+                    height = imagesarray[i].thumbHeight;
+                }
+            }
+            return{
+                width:width,
+                height:height
+            };
+            
+        }
+        
         
         
         
@@ -113,11 +167,7 @@ var pwd =
        
         
         
-        
-        //img.remove();
-        
-        
-    },
+    }
     
 
 }
